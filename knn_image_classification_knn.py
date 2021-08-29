c4635de69db26b29c7fae3be181cd4297d828ce8
Kethan Vegunta <kethan@vegunta.com>
2021-08-29 15:27:47 -0700

    update filenames and move to color image for knn

--- knn_image_classification_knn.py
+++ knn_image_classification_knn.py
@@ -15,36 +15,30 @@ images = {
 
 
 grayscale_images = {
-    character: [cv2.imread(os.path.join(datafolder, character, image), cv2.IMREAD_GRAYSCALE) for image in paths]
+    character: [cv2.imread(os.path.join(datafolder, character, image)) for image in paths]
     for character, paths in images.items()
 }
 
-print(grayscale_images["donald_duck"][0])
-
 
 labels = np.asarray([each_character for character, image_lst in grayscale_images.items() for each_character in len(image_lst) * [character]])
-data = np.asarray([image for character, image_lst in grayscale_images.items() for image in image_lst])
+data = np.asarray([image.flatten() for character, image_lst in grayscale_images.items() for image in image_lst])
 # print(labels)
 print(data)
 
-nsamples, nx, ny = data.shape
-reshaped_data = data.reshape((nsamples, nx * ny))
 # x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(reshaped_data, labels, test_size=0.001)
 
 clf = neighbors.KNeighborsClassifier()
 # Train the classifier
-clf.fit(reshaped_data, labels)
+clf.fit(data, labels)
 
 # Get the accuracy of the classifier basing on the test data
 # accuracy = clf.score(x_test, y_test)
 # print(accuracy)
 
-test_image = np.asarray([cv2.imread(os.path.join(datafolder, "apple.jpg"), cv2.IMREAD_GRAYSCALE)])
-nsamples, nx, ny = test_image.shape
-test_image_reshaped = test_image.reshape((nsamples, nx * ny))
-print(clf.predict(test_image_reshaped))
+test_image = cv2.imread(os.path.join(datafolder, "apple.jpg"))
+test_image = np.asarray([test_image.flatten()])
+print(clf.predict(test_image))
 
-test_image = np.asarray([cv2.imread(os.path.join(datafolder, "donald_duck.jpg"), cv2.IMREAD_GRAYSCALE)])
-nsamples, nx, ny = test_image.shape
-test_image_reshaped = test_image.reshape((nsamples, nx * ny))
-print(clf.predict(test_image_reshaped))
+test_image = cv2.imread(os.path.join(datafolder, "donald_duck.jpg"))
+test_image = np.asarray([test_image.flatten()])
+print(clf.predict(test_image))
